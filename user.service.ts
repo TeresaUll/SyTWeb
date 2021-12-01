117a663e9536d15b27f2c3949dea99e827514213
martaa163 <74718915+martaa163@users.noreply.github.com>
2021-12-01 16:50:37 +0100

    Update user.service.ts

--- user.service.ts
+++ user.service.ts
@@ -2,13 +2,21 @@ import {Injectable} from "@angular/core";
 import {Http, Response} from "@angular/http";
 import 'rxjs/add/operator/map';
 import {Jugador}  from "./jugador.model";
+import {Partida} from "./partida.model";
+
 
 @Injectable()
-export class UserService {
+export class Service {
   constructor(private http: Http) {}
 
-  getUser() {
-    return this.http.get('/api/user')
-      .map((res: Response) => res.json().response);
+  getJugador(): Observable<Jugador[]> {
+  return this.http.get('/api/user')
+    .map((res: Response) => res.json().response.map((jugador: Jugador) => new Jugador().deserialize(jugador)));
   }
+  
+  getPartida(): Observable<Partida[]> {
+  return this.http.get('/api/user')
+    .map((res: Response) => res.json().response.map((partida: Partida) => new Partida().deserialize(partida)));
+  }
+  
 }
